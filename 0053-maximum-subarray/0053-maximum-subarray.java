7d140ecc73946dc2659026d793c79ed8477f10c7
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2023-10-02 01:28:19 +0530

    Time: 1 ms (100.00%), Space: 57.7 MB (79.71%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,23 +1,34 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         //KADENS ALGORITHM
+//         if(nums.length==0)
+//             return 0;
+//         int cur = 0;
+//         int maxtillnow = nums[0];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             cur = cur + nums[i];
+//             if(cur>maxtillnow)
+//             {
+//                 maxtillnow = cur;
+//             }
+//             if(cur<=0)
+//             {
+//                 cur = 0;
+//             }
+//         }
+//         return maxtillnow
         if(nums.length==0)
             return 0;
-        int cur = 0;
-        int maxtillnow = nums[0];
-        for(int i=0;i<nums.length;i++)
+        int current=nums[0];
+        int maxSum=nums[0];
+        for(int i=1;i<nums.length;i++)
         {
-            cur = cur + nums[i];
-            if(cur>maxtillnow)
-            {
-                maxtillnow = cur;
-            }
-            if(cur<=0)
-            {
-                cur = 0;
-            }
+            current=Math.max(nums[i],current+nums[i]);
+           maxSum=Math.max(maxSum,current);
         }
-        return maxtillnow;
-}
+        return maxSum;
+    }
+// }
 }
 
