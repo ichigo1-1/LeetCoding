939b356f1ad25df0393766b22f1b95e9d87d58cd
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-01-20 18:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (13.99%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -13,11 +13,33 @@ class Solution {
 
         // }
         // return false;
-        double val=Math.sqrt(num);
-        int res=(int)val;
-        if((res*res)==num)
+
+                    // brute
+        // double val=Math.sqrt(num);
+        // int res=(int)val;
+        // if((res*res)==num)
+        // {
+        //     return true;
+        // }
+        // return false;
+
+                        // BINARY SEARCH APPROACH
+        long low=1;
+        long high=num;
+        while(low<=high)
         {
-            return true;
+            long mid=low+(high-low)/2;
+            if(mid*mid==num)
+            {
+                return true;
+            }
+            else if(mid*mid>num)
+            {
+                high=mid-1;
+            }
+            else if(mid*mid<num){
+                low=mid+1;
+            }
         }
         return false;
 
