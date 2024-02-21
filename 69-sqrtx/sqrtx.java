6d2d1340cab467b170935024bb6eaf340c2a4b4a
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-02-22 00:49:44 +0530

    Time: 1 ms (87.42%) | Memory: 41 MB (35.61%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -2,25 +2,28 @@ class Solution {
     public int mySqrt(int x) {
         long y=(long)x;
         long low=1;
-		long high=y;
-		long ans=0;
-		while(low<=high)
-		{
-			long mid=(low+high)/2;
-			long val=mid*mid;
-			if(val<=y)
-			{
-				ans=mid;
-				low=mid+1;
-			}
-			else
-			{
-				high=mid-1;
-			}
-
-		
-		}
-		return (int)ans;
+        long high=y;
+        long ans=0;
+        while(low<=high)
+        {
+            long mid=(low+high)/2;
+            long val=mid*mid;
+            if(val<=y)
+            {
+                ans=mid;
+                 low=mid+1;
+            }
+            else{
+                
+               
+                high=mid-1;
+            }
+        }
+        return (int)ans;
         
     }
 }
+
+
+
+
