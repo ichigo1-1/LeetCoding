c617b7efa013db372db9cf0906bc7d0e712c98e3
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-02-24 17:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (73.45%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,19 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
-         int low = 0;
-    int high = citations.length - 1;
-    int n = citations.length;
-    int ans = 0;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (n - mid <= citations[mid]) {
-            ans = n - mid; // Update with the count of papers, not citations
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+        int low=0;
+        int high=citations.length-1;
+        int n=citations.length-1;
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(n-mid+1<=citations[mid])
+            {
+                ans=n-mid+1;
+                //as right wala sorted hi hai
+                //we want to look up for elements with more h index kam value but more qualtity so well look on left;
+                high=mid-1;
+            }
+            else{
+               
+                low=mid+1;
+            }
         }
-    }
-    return ans;
+        return ans;
         
     }
 }
