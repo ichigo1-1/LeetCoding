66b632fafeedb5249ad337bc31bcf88d1450a812
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-02-25 03:10:17 +0530

    Time: 97 ms (11.74%) | Memory: 61.2 MB (27.84%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+    //     int[]res=new int[nums.length-k+1];
+    //     //store the next greater element for each el
+    //     Stack<Integer>st=new Stack<>();
+    //     int[]nge=new int[nums.length];
+    //     nge[nums.length-1]=nums.length;
+    //     st.push(nums.length-1);
+
+    //     for(int i=nums.length-2;i>=0;i--)
+    //     {
+    //         while(!st.isEmpty() && nums[i]>=nums[st.peek()])//chota logko hatate raho
+    //         {
+    //             st.pop();
+    //         }
+    //         if(st.isEmpty())
+    //         {
+    //             nge[i]=nums.length;
+    //         }
+    //         else{
+    //             nge[i]=st.peek();
+    //         }
+    //         st.push(i);
+    //     }
+
+    //     //now run loop and see
+    //     int ind=0;
+    //     int j=0;
+    //     for(int i=0;i<=nums.length-k;++i)
+    //     {
+    //         // int j=i;//to jump to nge
+    //          if(j < i){
+    //     j = i;
+    //   }
+    //         while(nge[j]<i+k)
+    //         {
+    //             j=nge[j];
+    //         }
+    //         res[ind++]=j;
+
+
+    //     }
+    //     return res;
+
+        int[] nge = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(arr.length - 1);
+        nge[arr.length - 1] = arr.length;
+        for (int i = arr.length - 2; i >= 0; --i) {
+            while (!st.isEmpty() && arr[i] >= arr[st.peek()]) 
+                st.pop();
+            if (st.isEmpty()) nge[i] = arr.length; 
+            else nge[i] = st.peek();
+            st.push(i);
+        }
+
+        int[] ans = new int[arr.length - k + 1];
+        int j = 0; // To travel in nge
+        for (int i = 0; i <= arr.length - k; ++i) {
+            if (j < i) j = i;
+            while (nge[j] < i + k) j = nge[j];
+            ans[i] = arr[j];
+        }
+        
+        return ans;
+        
+    }
+}
