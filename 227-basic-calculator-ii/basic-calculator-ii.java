4fa7829f76de9bd3ca36639260f73a9c1de53a32
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-02-25 14:22:10 +0530

    Time: 39 ms (18.19%) | Memory: 45.9 MB (66.08%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -46,137 +46,143 @@ class Solution {
 
         
 
-        // Stack<Integer>numbers=new Stack<>();
-        // Stack<Character>symbols=new Stack<>();
+        Stack<Integer>numbers=new Stack<>();
+        Stack<Character>symbols=new Stack<>();
 
     
 
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     char ch=s.charAt(i);
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
 
-        //     if(ch=='(')
-        //     {
-        //         symbols.push(ch);
-        //     }
-        //     else if(ch==')')
-        //     {
-        //         while(symbols.peek()!='(')
-        //         {
-        //            char optr = symbols.pop();
+            if(ch=='(')
+            {
+                symbols.push(ch);
+            }
+            else if(ch==')')
+            {
+                while(symbols.peek()!='(')
+                {
+                   char optr = symbols.pop();
 
-        //            int v1=numbers.pop();
-        //            int v2=numbers.pop();
+                   int v1=numbers.pop();
+                   int v2=numbers.pop();
 
-        //            int opv=operation(v2,v1,optr);
-        //            numbers.push(opv);
-        //         }
-        //         //remove the ( bracket
-        //         symbols.pop();
-        //     }
-        //     else if(Character.isDigit(ch))
-        //     {
-        //         numbers.push(ch-'0');
-        //     }
-        //     else if(ch=='+' || ch=='-' || ch=='*' || ch=='/')
-        //     {
-        //         while(symbols.size()>0 && symbols.peek()!='(' && precedence(ch)<=precedence(symbols.peek()))
-        //         {
-        //             //do the same job were doing with )
-        //             char optr = symbols.pop();
+                   int opv=operation(v2,v1,optr);
+                   numbers.push(opv);
+                }
+                //remove the ( bracket
+                symbols.pop();
+            }
+            else if(Character.isDigit(ch))
+            {
+                int num=0;
+                while(i<s.length() && Character.isDigit(s.charAt(i)))
+                {
+                    num=num*10+(s.charAt(i)-'0');
+                    i++;
+                }
+                //decrement as i++ hone ka bad i ka ek value jada hoga
+                i--;
+                numbers.push(num);
+            }
+            else if(ch=='+' || ch=='-' || ch=='*' || ch=='/')
+            {
+                while(symbols.size()>0 && symbols.peek()!='(' && precedence(ch)<=precedence(symbols.peek()))
+                {
+                    //do the same job were doing with )
+                    char optr = symbols.pop();
 
-        //            int v1=numbers.pop();
-        //            int v2=numbers.pop();
+                   int v1=numbers.pop();
+                   int v2=numbers.pop();
 
-        //            int opv=operation(v2,v1,optr);
-        //            numbers.push(opv);
+                   int opv=operation(v2,v1,optr);
+                   numbers.push(opv);
 
-        //         }
-        //         //push the symbol
-        //         symbols.push(ch);
+                }
+                //push the symbol
+                symbols.push(ch);
 
-        //     }
-        // }
-        // //might be a case where the symbols are not empty// so we do operation manually until size ==0
-        // if(symbols.size()<0)
-        // {
-        //     return Integer.parseInt(s);
-        // }
-        // while(symbols.size()>0)
-        // {
-        //     char optr = symbols.pop();
+            }
+        }
+        //might be a case where the symbols are not empty// so we do operation manually until size ==0
 
-        //            int v1=numbers.pop();
-        //            int v2=numbers.pop();
+       
+        while(symbols.size()>0)
+        {
+            char optr = symbols.pop();
 
-        //            int opv=operation(v2,v1,optr);
-        //            numbers.push(opv);
+                   int v1=numbers.pop();
+                   int v2=numbers.pop();
 
-        // }
-        // //numbers ka peek me jo last element bachega wahi ans hai
+                   int opv=operation(v2,v1,optr);
+                   numbers.push(opv);
+
+        }
+        //numbers ka peek me jo last element bachega wahi ans hai
         
 
-        // return numbers.peek();
+        return numbers.peek();
 
 
 
 
-         Stack<Integer> numbers = new Stack<>();
-    Stack<Character> symbols = new Stack<>();
+    //      Stack<Integer> numbers = new Stack<>();
+    // Stack<Character> symbols = new Stack<>();
 
-    for (int i = 0; i < s.length(); i++) {
-        char ch = s.charAt(i);
+    // for (int i = 0; i < s.length(); i++) {
+    //     char ch = s.charAt(i);
 
-        if (ch == '(') {
-            symbols.push(ch);
-        } else if (ch == ')') {
-            while (symbols.peek() != '(') {
-                char optr = symbols.pop();
+    //     if (ch == '(') {
+    //         symbols.push(ch);
+    //     } else if (ch == ')') {
+    //         while (symbols.peek() != '(') {
+    //             char optr = symbols.pop();
 
-                int v1 = numbers.pop();
-                int v2 = numbers.pop();
+    //             int v1 = numbers.pop();
+    //             int v2 = numbers.pop();
 
-                int opv = operation(v2, v1, optr);
-                numbers.push(opv);
-            }
-            // Remove the '(' bracket
-            symbols.pop();
-        } else if (Character.isDigit(ch)) {
-            // Handle multi-digit numbers
-            int num = 0;
-            while (i < s.length() && Character.isDigit(s.charAt(i))) {
-                num = num * 10 + (s.charAt(i) - '0');
-                i++;
-            }
-            // Decrease i to account for the extra increment in the loop
-            i--;
-            numbers.push(num);
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!symbols.isEmpty() && symbols.peek() != '(' && precedence(ch) <= precedence(symbols.peek())) {
-                char optr = symbols.pop();
+    //             int opv = operation(v2, v1, optr);
+    //             numbers.push(opv);
+    //         }
+    //         // Remove the '(' bracket
+    //         symbols.pop();
+    //     } else if (Character.isDigit(ch)) {
+    //         // Handle multi-digit numbers
+    //         int num = 0;
+    //         while (i < s.length() && Character.isDigit(s.charAt(i))) {
+    //             num = num * 10 + (s.charAt(i) - '0');
+    //             i++;
+    //         }
+    //         // Decrease i to account for the extra increment in the loop
+    //         i--;
+    //         numbers.push(num);
+    //     } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+    //         while (!symbols.isEmpty() && symbols.peek() != '(' && precedence(ch) <= precedence(symbols.peek())) {
+    //             char optr = symbols.pop();
 
-                int v1 = numbers.pop();
-                int v2 = numbers.pop();
+    //             int v1 = numbers.pop();
+    //             int v2 = numbers.pop();
 
-                int opv = operation(v2, v1, optr);
-                numbers.push(opv);
-            }
-            // Push the symbol
-            symbols.push(ch);
-        }
-    }
+    //             int opv = operation(v2, v1, optr);
+    //             numbers.push(opv);
+    //         }
+    //         // Push the symbol
+    //         symbols.push(ch);
+    //     }
+    // }
 
-    // Handle the case where the symbols stack is not empty after the loop
-    while (!symbols.isEmpty()) {
-        char optr = symbols.pop();
-        int v1 = numbers.pop();
-        int v2 = numbers.pop();
-        int opv = operation(v2, v1, optr);
-        numbers.push(opv);
-    }
+    // // Handle the case where the symbols stack is not empty after the loop
+    // while (!symbols.isEmpty()) {
+    //     char optr = symbols.pop();
+    //     int v1 = numbers.pop();
+    //     int v2 = numbers.pop();
+    //     int opv = operation(v2, v1, optr);
+    //     numbers.push(opv);
+    // }
 
-    // The result will be at the top of the numbers stack
-    return numbers.peek();
+    // // The result will be at the top of the numbers stack
+    // return numbers.peek();
 
 
 
