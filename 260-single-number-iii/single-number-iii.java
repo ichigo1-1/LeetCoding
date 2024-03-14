ceae5d9786190079b055d7c80786816ed90dba5a
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-15 01:15:34 +0530

    Time: 7 ms (19.87%) | Memory: 45.3 MB (86.78%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,27 +1,44 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int[]res=new int[2];
-        int xor=0;
-        for(int it:nums)
+    //     int[]res=new int[2];
+    //     int xor=0;
+    //     for(int it:nums)
+    //     {
+    //         xor^=it;
+    //     }
+    //     //find the rightmost bit
+    //     int rightbit=xor&~(xor-1);
+    //     int set1=0;
+    //     int set2=0;
+    //     for(int it:nums)
+    //     {
+    //         if((rightbit & it)==0)
+    //         {
+    //             set1^=it;
+    //         }
+    //         else{
+    //             set2^=it;
+    //         }
+    //     }
+    //   res[0]=set1;
+    //   res[1]=set2;
+    //     return res;
+
+    HashMap<Integer,Integer>hm=new HashMap<>();
+    int[]res=new int[2];
+    for(int it:nums)
+    {
+        hm.put(it,hm.getOrDefault(it,0)+1);
+    }
+    int i=0;
+    for(int it:hm.keySet())
+    {
+        if(hm.get(it)==1)
         {
-            xor^=it;
+            res[i]=it;
+            i++;
         }
-        //find the rightmost bit
-        int rightbit=xor&~(xor-1);
-        int set1=0;
-        int set2=0;
-        for(int it:nums)
-        {
-            if((rightbit & it)==0)
-            {
-                set1^=it;
-            }
-            else{
-                set2^=it;
-            }
-        }
-      res[0]=set1;
-      res[1]=set2;
-        return res;
+    }
+    return res;
     }
 }
