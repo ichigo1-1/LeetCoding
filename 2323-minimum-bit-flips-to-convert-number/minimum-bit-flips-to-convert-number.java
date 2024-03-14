95a338197ad25ad4b28af4e63aa38b566734da80
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-14 22:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (35.11%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -4,11 +4,15 @@ class Solution {
         int count=0;
         while(n>1)
         {
-            if(n%2==1)
-            {
-                count++;
-            }
-            n=n/2;
+            // if(n%2==1)
+            // {
+            //     count++;
+            // }
+
+            count+=n&1;//to check odd or even agar odd toh 1 else 0
+            // n=n/2;
+            //can use right shift
+            n=n>>1;
         }
         if(n==1)
         {
