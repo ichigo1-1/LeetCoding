a718df369f816a98a6b83b902178e3b0bd99abc9
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-16 02:31:39 +0530

    Time: 1 ms (69.47%) | Memory: 40.9 MB (48.33%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -44,22 +44,26 @@
 //     }
 // }
 class Solution {
-       public static int divide(int dividend, int divisor) {
-        if (Integer.MIN_VALUE == dividend && divisor == -1)
-            return Integer.MAX_VALUE;
-        int ans = 0;
-        int a = Math.abs(dividend);
-        int b = Math.abs(divisor);
-        while (a - b >= 0) {
-            int temp = b;
-            int count = 1;
-            while (a - (temp << 1) >= 0) {
-                temp <<= 1;
-                count <<= 1;
-            }
-            a -= temp;
-            ans += count;
+    public int divide(int dividend, int divisor) {
+        long quotient = 0;
+        int sign = 1;
+        int max = Integer.MAX_VALUE,  min = Integer.MIN_VALUE;
+        if(dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0)sign = -1;
+        long dd = (long)dividend , dv = (long)divisor;
+        dd = Math.abs(dd);dv = Math.abs(dv);
+        while(dd >= dv){
+            int shift = 0;
+            while(dd >= (dv<<shift))shift++;
+            quotient += (long)1l<<(shift-1);
+            // System.out.println(quotient+"-"+shift);
+            dd -= dv<<(shift-1);
         }
-        return (dividend > 0) == (divisor > 0) ? ans : -ans;
+        if(sign == 1){
+            if(quotient > max)return max;
+            return (int)quotient;
+        }else{
+            if((~quotient)+1 < min)return min; // ~quotient+1 = -quotient
+            return (int)(~quotient)+1;
+        }
     }
 }
