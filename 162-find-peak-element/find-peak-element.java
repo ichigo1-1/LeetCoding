4379bf199e9dc410004cf1c86dd6d2f87804707b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-16 17:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (82.33%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,43 +1,48 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int n=nums.length;
-        // if(n==1)
+    public int findPeakElement(int[] arr) {
+        // if(nums.length==1)
         //     return 0;
-        // if(nums[0]>=nums[1])
+        // if(nums[0]>nums[1])
         //     return 0;
-        // if(nums[n-1]>=nums[n-2])
-        //     return n-1;
-        // for(int i=1;i<n-1;i++)
-        // {
-        //     if(nums[i]>=nums[i+1] && nums[i]>=nums[i-1])
+        // if(nums[nums.length-1]>nums[nums.length-2])
+        //     return nums.length-1;
+
+        //  int low=1;
+        //  int high=nums.length-2;
+        //  while(low<=high)
+        //  {
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
         //     {
-        //         return i;
+        //         return mid;
         //     }
-        // }
-        // return 0;
-        
-        // Binary search
-        
-        int left=0;
-        int right=n-1;
-        while(left<right)
-        {
-            int mid=left+(right-left)/2;
-            //think of the mountain example
-            
-            // if(nums[mid]>=nums[mid+1])
-            //     right=mid;
-            // else
-            //     left=mid+1;
-        if(nums[mid]>=nums[mid+1])
-        {
-            right=mid;
+        //     else if(nums[mid]>nums[mid-1])//incr curve
+        //     {
+        //         low=mid+1;
+        //     }
+        //     else if(nums[mid]>nums[mid+1]){
+        //         high=mid-1;
+        //     }
+        //  }   
+        // return -1;
+        int n = arr.length;
+
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        int i=1;
+        int j=n-2;
+
+        while(i<=j){
+            int mid = i+(j-i)/2;
+
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+            else if(arr[mid]<arr[mid-1]) j = mid-1;
+            else if(arr[mid]<arr[mid+1]) i = mid+1;
         }
-        else{
-            left=mid+1;
-        }
-        }
-        return left;
+
+        return -1;
         
     }
 }
