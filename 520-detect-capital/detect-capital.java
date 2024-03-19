b8bc808523d300f8abc70f289a6eecf31b2499c0
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-03-19 10:53:42 +0530

    Time: 1 ms (88.57%) | Memory: 41.9 MB (26.77%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count=0;
+        for(char ch:word.toCharArray())
+        {
+            if(ch>='A' && ch<='Z')
+            {
+                count++;
+            }
+        }
+        if(count==1 && word.charAt(0)>='A' && word.charAt(0)<='Z')
+        {
+            return true;
+        }
+        if(count==word.length())
+        {
+            return true;
+        }
+        if(count==0)
+        {
+            return true;
+        }
+        return false;
+        // return word.matches("[A-Z]+") ||     // All capitals
+        // word.matches("[a-z]+") ||             // All small letters
+        // word.matches("[A-Z]{1}[a-z]+");  // First capital, all small
+        
+        
+
+
+        
+    }
+}
