1a0def53c2d2989f0741594c2a158cdf51e32406
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-04-06 00:56:20 +0530

    Time: 29 ms (81.11%) | Memory: 51.4 MB (68.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -22,23 +22,69 @@ class Solution {
         // return ans;
 
         //BETTER  APPROACH
-        Set<List<Integer>>set=new HashSet<>();
+        // Set<List<Integer>>set=new HashSet<>();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     Set<Integer>hashset=new HashSet<>();
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         int third=-(nums[i]+nums[j]);///basic maths formula
+        //         if(hashset.contains(third))
+        //         {
+        //             List<Integer>temp=Arrays.asList(nums[i],nums[j],third);
+        //             Collections.sort(temp);
+        //             set.add(temp);
+        //         }
+        //         hashset.add(nums[j]);
+        //     }
+        // }
+        // List<List<Integer>>ans=new ArrayList<>(set);
+        // return ans;
+
+
+        //OPTIMAL SOLUTION
+        //WELL SORT THE ARRAY AND DO THE 2 POUNTER APPROACH STRIVER
+        Arrays.sort(nums);
+        List<List<Integer>>ans=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            Set<Integer>hashset=new HashSet<>();
-            for(int j=i+1;j<nums.length;j++)
+            if(i>0 && nums[i]==nums[i-1])
             {
-                int third=-(nums[i]+nums[j]);///basic maths formula
-                if(hashset.contains(third))
-                {
-                    List<Integer>temp=Arrays.asList(nums[i],nums[j],third);
-                    Collections.sort(temp);
-                    set.add(temp);
-                }
-                hashset.add(nums[j]);
+                continue;
             }
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)//SUM KAM ARAI TO BARHAO
+                {
+                    j++;
+                }
+                else if(sum>0)//JADA ARAI TO GHATAO
+                {
+                    k--;
+                }
+                else{
+                    List<Integer>temp=Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //now check if same or not
+                    //if same move until dif element//also check boundary j<k
+                    while(j<k && nums[j]==nums[j-1])
+                    {
+                        j++;
+                    }
+                     while(j<k && nums[k]==nums[k+1])
+                    {
+                        k--;
+                    }
+
+                }
+            }
+            
         }
-        List<List<Integer>>ans=new ArrayList<>(set);
         return ans;
 
         
