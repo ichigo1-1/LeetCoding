d357916179794eaff2d0836ca5b1feb2a91ffae4
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-01 22:20:18 +0530

    Time: 6 ms (84.10%) | Memory: 45.1 MB (39.55%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,24 +1,44 @@
 class Solution {
     public int firstUniqChar(String S) {
-         for (int i = 0; i < S.length(); i++) {
-            char currentChar = S.charAt(i);
-            boolean foundDuplicate = false;
+        //  for (int i = 0; i < S.length(); i++) {
+        //     char currentChar = S.charAt(i);
+        //     boolean foundDuplicate = false;
             
-            // Check if the current character appears elsewhere in the string
-            for (int j = 0; j < S.length(); j++) {
-                if (i != j && currentChar == S.charAt(j)) {
-                    foundDuplicate = true;
-                    break;
-                }
-            }
+        //     // Check if the current character appears elsewhere in the string
+        //     for (int j = 0; j < S.length(); j++) {
+        //         if (i != j && currentChar == S.charAt(j)) {
+        //             foundDuplicate = true;
+        //             break;
+        //         }
+        //     }
             
-            // If the current character is non-repeating, return it
-            if (!foundDuplicate) {
-                return i;
+        //     // If the current character is non-repeating, return it
+        //     if (!foundDuplicate) {
+        //         return i;
+        //     }
+        // }
+        
+        // // If no non-repeating character is found, return '$'
+        // return -1;
+
+
+        //Approach 2
+        //maintain a map with array
+        int[]freq=new int[26];
+        for(int i=0;i<S.length();i++)
+        {
+            char ch=S.charAt(i);
+            freq[ch-'a']++;
+        }
+        //again traverse
+        for(int j=0;j<S.length();j++)
+        {
+            char ch = S.charAt(j);
+            if(freq[ch-'a']==1)
+            {
+                return j;
             }
         }
-        
-        // If no non-repeating character is found, return '$'
         return -1;
         
     }
