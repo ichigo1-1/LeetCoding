9e0e9cacf7d563f5746fb6e96e8cfaadbf3e9a45
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-01 23:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (67.12%) - LeetSync

--- 916-decoded-string-at-index/decoded-string-at-index.java
+++ 916-decoded-string-at-index/decoded-string-at-index.java
@@ -8,7 +8,7 @@ class Solution {
 
       
         // //step 1:
-         int n = s.length();
+        int n = s.length();
         long len = 0;
 
         for(char ch : s.toCharArray()){
@@ -22,15 +22,16 @@ class Solution {
         
 
         //step 2: Travserse from end as ans may always lie in the back
+        //beacuse we dont need to build the whole string if im using modulo
          for(int i=n-1;i>=0;i--){
             char c = s.charAt(i);
-            if(Character.isDigit(c)){
+            if(Character.isDigit(c)){//it doesnt matter if modulo comes out to be 0 also as it is a digit it will never be my answer
                 len = len / (c - '0'); 
                 k %= len;
             }
             else {
                 if(k == 0 || k == len) return Character.toString(c);
-                len --;
+                len --;//always reduce by 1 in case of a letter
             }
         }
         return "";
