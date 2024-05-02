0680e490d359824709548209bc2bae5c107203eb
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-03 00:01:28 +0530

    Time: 2 ms (42.09%) | Memory: 44.5 MB (75.81%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
@@ -1,17 +1,37 @@
 class Solution {
     public long countSubstrings(String s, char c) {
 
-        int n = s.length();
-        long ans = 0;
-        long count = 0;
 
-        for(int i=0; i<n; i++){
-            if(s.charAt(i) == c){
+        //add current c and the previous occurences of c
+        long count=0;
+        long ans=0;
+        for(char ch:s.toCharArray())
+        {
+            if(ch==c)
+            {
+                //current ko add to karo hi and add the prev occurenecs
+                ans+=(1+count);
+                //as ive seen one c so increase count for prev occurences
                 count++;
-                ans += count;
             }
         }
         return ans;
+
+
+        //METHOD 2
+        //AS WE HAVE TO START AND EN
+
+        // int n = s.length();
+        // long ans = 0;
+        // long count = 0;
+
+        // for(int i=0; i<n; i++){
+        //     if(s.charAt(i) == c){
+        //         count++;
+        //         ans += count;
+        //     }
+        // }
+        // return ans;
         
     }
 }
