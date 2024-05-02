f1d31ed850109ef73a9c5fdf613fbdef6b9110d6
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-03 00:05:14 +0530

    Time: 2 ms (42.09%) | Memory: 44.3 MB (91.97%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
@@ -19,8 +19,16 @@ class Solution {
 
 
         //METHOD 2
-        //AS WE HAVE TO START AND EN
 
+        //AS WE HAVE TO START AND END WITH SAME CHAR WE CAN USE COMBINATION LIKE 
+        //4C2 AND THEN COUNT THE INDVIDUAL OCCURENECS
+        //EXAMPLE:-ABADA  C=A
+        //AS WE NEED TO FORM SUBS STARTING AND ENDING WITH A 
+        //POSSIBLE COMB COULD BE A____A (3C2)ARRANGING 3 A'S IN 2 POSIIYTON + INDIVIDUAL AS
+
+
+
+        //EASY METHOD
         // int n = s.length();
         // long ans = 0;
         // long count = 0;
