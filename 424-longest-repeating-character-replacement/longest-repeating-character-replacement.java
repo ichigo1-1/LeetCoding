f251443131281487b76986e0a0e8fac503b649be
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-06 00:49:19 +0530

    Time: 1515 ms (5.02%) | Memory: 42.5 MB (52.50%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,10 +1,10 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[256]; // Assuming ASCII characters
-int max = 0;
-int maxFreq = 0;
+    int[] freq = new int[26]; // Assuming ASCII characters
+    int max = 0;
+    int maxFreq = 0;
 
-for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < s.length(); i++) {
     Arrays.fill(freq, 0); // Resetting frequency array for each i
     for (int j = i; j < s.length(); j++) {
         freq[s.charAt(j) - 'A']++;
