60e55358ae874801dc805f98f109aefb01f12888
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-18 18:01:09 +0530

    Time: 2 ms (53.15%) | Memory: 47 MB (77.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,82 +1,45 @@
-// class Solution {
-
-//     public void dfs(int start,List<List<Integer>> adj,boolean[]vis)
-//     {
-//         vis[start]=true;
-//         for(int it:adj.get(start))
-//         {
-//             if(vis[it]==false)
-//             {
-//                 dfs(it,adj,vis);
-//             }
-//         }
-//     }
-//     public int findCircleNum(int[][] isConnected) {
-//         //convert to list
-//         int n=isConnected.length;
-
-//         List<List<Integer>> adjacencyList = new ArrayList<>();
-//         for (int i = 0; i < n; i++) {
-//             adjacencyList.add(new ArrayList<>());
-//         }
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 if (isConnected[i][j] == 1) {
-//                     adjacencyList.get(i).add(j);
-                   
-//                 }
-//             }
-//         }
-//         boolean[]visited=new boolean[n+1];
-//         int count=0;
-  
-//         for(int i=0;i<n;i++)
-//         {
-//             if(visited[i]==false)
-//             {
-//                 count++;
-//                 dfs(0,adjacencyList,visited);
-//             }
-//         }
-//         return count;
-
-
-        
-//     }
-// }
-
-import java.util.*;
-
 class Solution {
-
-    public void dfs(int start, List<List<Integer>> adj, boolean[] vis) {
-        vis[start] = true;
-        for (int it : adj.get(start)) {
-            if (!vis[it]) {
-                dfs(it, adj, vis);
+    public void dfs(int node,boolean[]vis,ArrayList<ArrayList<Integer>>arr)
+    {
+        vis[node]=true;
+        //visit adj
+        for(int it:arr.get(node))
+        {
+            if(vis[it]==false)
+            {
+                dfs(it,vis,arr);
             }
         }
     }
-
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-
-        List<List<Integer>> adjacencyList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adjacencyList.add(new ArrayList<>());
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    adjacencyList.get(i).add(j);
+        int count=0;
+        ArrayList<ArrayList<Integer>>arr=new ArrayList<>();
+        int n=isConnected.length;
+        for(int i=0;i<n;i++)
+        {
+            arr.add(new ArrayList<>());
+        }
+        
+        //now traverse
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    //add to list
+                    arr.get(i).add(j);
                 }
             }
         }
-        boolean[] visited = new boolean[n]; // Fix array size
-        int count = 0;
-
-        for (int i = 0; i < n; i++) { 
-            if (!visited[i]) { 
+        //now traverse using dfs
+        boolean[]vis=new boolean[n];
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==false)
+            {
+                dfs(i,vis,arr);
                 count++;
-                dfs(i, adjacencyList, visited); // Fix parameter to pass current node
             }
         }
         return count;
