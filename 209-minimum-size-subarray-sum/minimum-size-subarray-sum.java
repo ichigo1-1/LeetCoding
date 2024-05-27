1118bdc5a6766587cbd1b9a8ae802ab915bb351f
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-28 03:06:32 +0530

    Time: 1 ms (99.80%) | Memory: 58.1 MB (34.68%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -3,16 +3,28 @@ class Solution {
        int len = Integer.MAX_VALUE; // Initialize len to a large value
         int i = 0;
         int sum = 0;
+        int j=0;
         int n = nums.length;
 
-        for (int j = 0; j < n; j++) {
-            sum += nums[j];
+        // for (int j = 0; j < n; j++) {
+        //     sum += nums[j];
             
-            while (sum >= target) {
-                len = Math.min(len, j - i + 1);
-                sum -= nums[i];
+        //     while (sum >= target) {
+        //         len = Math.min(len, j - i + 1);
+        //         sum -= nums[i];
+        //         i++;
+        //     }
+        // }
+        while(j<n)
+        {
+            sum+=nums[j];
+            while(sum>=target)
+            {
+                len=Math.min(len,j-i+1);
+                sum-=nums[i];
                 i++;
             }
+            j++;
         }
 
         // If len was never updated, return 0 (indicating no valid subarray found)
