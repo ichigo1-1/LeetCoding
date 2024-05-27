1ee3c7dbad0dd26992b481e6fed8b1b7591e5bdf
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-05-27 16:18:44 +0530

    Time: 2 ms (47.70%) | Memory: 41.3 MB (29.54%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,22 +1,38 @@
 class Solution {
-    public int counts(int []nums,int x)
+    public int lowerbound(int []arr,int x)
     {
-        int count=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]>=x)
-            {
-                count++;
+        // int count=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]>=x)
+        //     {
+        //         count++;
+        //     }
+        // }
+        // return count;
+       int low = 0, high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
             }
         }
-        return count;
+        return ans;
     }
     public int specialArray(int[] nums) {
         int n=nums.length;
-       
+        Arrays.sort(nums);
         for(int x=0;x<=n;x++)
         {
-            int counter=counts(nums,x);
+            int idx=lowerbound(nums,x);
+            int counter=n-idx;
             if(counter==x)
             {
                 return x;
