f66689922cc9227e2bb23d5be39f8cac0db18c4c
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-02 16:48:40 +0530

    Time: 122 ms (100.00%) | Memory: 46 MB (100.00%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
@@ -0,0 +1,74 @@
+// class Solution {
+//     class Pair {
+//         int ind;
+//         char ch;
+
+//         Pair(int ind, char ch) {
+//             this.ch = ch;
+//             this.ind = ind;
+//         }
+
+//         public int getFirst() {
+//             return ind;
+//         }
+
+//         public char getChar() {
+//             return ch;
+//         }
+//     }
+
+//     public String clearStars(String s) {
+//         PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> {
+//             if (p1.ch == p2.ch) {
+//                 return Integer.compare(p1.ind, p2.ind);
+//             } else {
+//                 return Character.compare(p1.ch, p2.ch);
+//             }
+//         });
+
+//         int n = s.length();
+//         char[] arr = s.toCharArray();
+
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == '*') {
+//                 // Ensure the priority queue is not empty before polling
+//                 if (!pq.isEmpty()) {
+//                     int index = pq.poll().getFirst();
+//                     arr[index] = '*';
+//                 }
+//             } else {
+//                 pq.add(new Pair(i, arr[i]));
+            
+//         }
+
+//         // Create the result by ignoring '*'
+//         StringBuilder sb = new StringBuilder();
+//         for (char ch : arr) {
+//             if (ch != '*') {
+//                 sb.append(ch);
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+class Solution {
+     public String clearStars(String s) {
+        StringBuilder sb=new StringBuilder();
+        PriorityQueue<Character>pq=new PriorityQueue<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){
+                char c=pq.poll();
+                int minind=sb.lastIndexOf(c+"");
+                sb.deleteCharAt(minind);
+            }
+            else{
+                sb.append(s.charAt(i));
+                pq.offer(s.charAt(i));
+            }
+        }
+        return sb.toString();
+
+     }
+     }
