afc2b065a25b54dc8eb7adce02afb7327d8739a2
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-04 04:18:08 +0530

    Time: 2 ms (71.03%) | Memory: 41.4 MB (56.82%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,27 +1,64 @@
 class Solution {
     public String customSortString(String order, String s) {
-        int[]freq=new int[26];
-        for(char ch:s.toCharArray())
-        {
-            freq[ch-'a']++;
+        // int[]freq=new int[26];
+        // for(char ch:s.toCharArray())
+        // {
+        //     freq[ch-'a']++;
+        // }
+        // StringBuilder sb=new StringBuilder();
+        // //jbtk order ka char milta hai tbtk append karo
+        // for(char ch:order.toCharArray())
+        // {
+        //     while(freq[ch-'a']-->0)
+        //     {
+        //         sb.append(ch);
+        //     }
+        // }
+        // //now add the rest
+        // for(char ch:s.toCharArray())
+        // {
+        //     while(freq[ch-'a']-->0)
+        //     {
+        //         sb.append(ch);
+        //     }
+        // }
+        // return sb.toString();
+
+
+
+
+        ///USING MAP
+        HashMap<Character, Integer> freq = new HashMap<>();
+        
+        // Count the frequency of each character in s
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
-        StringBuilder sb=new StringBuilder();
-        //jbtk order ka char milta hai tbtk append karo
-        for(char ch:order.toCharArray())
-        {
-            while(freq[ch-'a']-->0)
-            {
-                sb.append(ch);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // Append characters in the order defined by the order string
+        for (char ch : order.toCharArray()) {
+            if (freq.containsKey(ch)) {
+                int count = freq.get(ch);
+                for (int i = 0; i < count; i++) {
+                    sb.append(ch);
+                }
+                freq.remove(ch); // Remove the character once it's fully appended
             }
         }
-        //now add the rest
-        for(char ch:s.toCharArray())
-        {
-            while(freq[ch-'a']-->0)
-            {
-                sb.append(ch);
+        
+        // Append the remaining characters that were not in order
+        for (char ch : s.toCharArray()) {
+            if (freq.containsKey(ch)) {
+                int count = freq.get(ch);
+                for (int i = 0; i < count; i++) {
+                    sb.append(ch);
+                }
+                freq.remove(ch); // Remove the character once it's fully appended
             }
         }
+        
         return sb.toString();
     }
 }
