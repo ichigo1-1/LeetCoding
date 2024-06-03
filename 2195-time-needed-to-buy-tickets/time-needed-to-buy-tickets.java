cd6779000b2b03b04994112104d66c79b39f3cb2
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-04 03:16:58 +0530

    Time: 2 ms (45.31%) | Memory: 40.9 MB (83.10%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,28 +1,42 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int n=tickets.length;
-        Queue<Integer>que=new LinkedList<>();
-        for(int i=0;i<n;i++)
-        {
-            que.add(i);//adding the indexes here
-        }
+        // Queue<Integer>que=new LinkedList<>();
+        // for(int i=0;i<n;i++)
+        // {
+        //     que.add(i);//adding the indexes here
+        // }
         int time=0;
-        while(!que.isEmpty())
-        {
-            time++;
-            int front=que.poll();
-            tickets[front]--;
+        // while(!que.isEmpty())
+        // {
+        //     time++;
+        //     int front=que.poll();
+        //     tickets[front]--;
 
-            //case
-            if(k==front && tickets[front]==0)
-            {
-                return time;
-            }
-            if(tickets[front]!=0)//agar 0 hoga then no need to add
-            {
-                que.add(front);
+        //     //case
+        //     if(k==front && tickets[front]==0)
+        //     {
+        //         return time;
+        //     }
+        //     if(tickets[front]!=0)//agar 0 hoga then no need to add
+        //     {
+        //         que.add(front);
+        //     }
+        // }
+        // return time;
+        while (tickets[k] > 0) {
+            for (int i = 0; i < n; i++) {
+
+                if (tickets[i] != 0) {
+                    tickets[i]--;
+                    time++;
+                }
+
+                if (tickets[k] == 0)
+                    return time;
             }
         }
+
         return time;
         
     }
