3255a3ca3cc9e6c051ad9ddebb8fc3f378773ea1
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-07 02:20:02 +0530

    Time: 24 ms (74.58%) | Memory: 46.3 MB (50.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -2,22 +2,39 @@ class Solution {
     public int subarraySum(int[] a, int k) {
         
                         //BRUTE FORCE APPROACH 
-        int count=0;
-        for(int i=0;i<a.length;i++)
-        {
-            int sum=0;
-            for(int j=i;j<a.length;j++)
-            {
-                sum+=a[j];
-                if(sum==k)
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
+        // int count=0;
+        // for(int i=0;i<a.length;i++)
+        // {
+        //     int sum=0;
+        //     for(int j=i;j<a.length;j++)
+        //     {
+        //         sum+=a[j];
+        //         if(sum==k)
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
         
         //OPTIMIZED SOLUTION
+    int j = 0;
+    int n = a.length;
+    int count = 0;
+    int sum = 0;
+    HashMap<Integer,Integer>hm=new HashMap<>();
+    hm.put(0,1);//the current sum=0 for edge cases wex:-[-1,1,0]
+    while (j < n) {
+        sum+=a[j];
+        if(hm.containsKey(sum-k))
+        {
+            count+=hm.get(sum-k);
+        }
+        hm.put(sum,hm.getOrDefault(sum,0)+1);
+        j++;
+    }
+
+    return count;
        
 
         
