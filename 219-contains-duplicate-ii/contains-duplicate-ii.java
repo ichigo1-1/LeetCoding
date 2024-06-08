4ccd6dfdbc38928de7e24a861b224a519025a96b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-08 16:45:00 +0530

    Time: 20 ms (40.63%) | Memory: 56.7 MB (55.81%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -15,19 +15,43 @@ class Solution {
         //     }
         // }
         // return false;
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        // HashMap<Integer,Integer>hm=new HashMap<>();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(hm.containsKey(nums[i]))
+        //     {
+        //         //the occurence is found now
+        //         //check the index difference
+        //         if(i-hm.get(nums[i])<=k)
+        //         {
+        //             return true;
+        //         }
+        //     }
+        //     hm.put(nums[i],i);
+        // }
+        // return false;
+        int i=0;
+        int j=0;
+        HashSet<Integer>set=new HashSet<>();
+        while(j<nums.length)
         {
-            if(hm.containsKey(nums[i]))
-            {
-                //the occurence is found now
-                //check the index difference
-                if(i-hm.get(nums[i])<=k)
-                {
-                    return true;
-                }
-            }
-            hm.put(nums[i],i);
+            // Check if the current number is already in the set
+    if (set.contains(nums[j])) {
+        return true;
+    }
+    
+    // Add the current number to the set
+    set.add(nums[j]);
+    
+    // Shrinking time: ensure the window size is at most k
+    if (Math.abs(i - j) >= k) {
+        set.remove(nums[i]);
+        i++;
+    }
+    
+    // Move to the next element
+    j++;
+
         }
         return false;
     }
