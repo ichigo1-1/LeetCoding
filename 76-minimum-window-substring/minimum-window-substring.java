8448a647c1892fe81df7ab141030721a03c3cb60
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-08 16:23:29 +0530

    Time: 18 ms (51.09%) | Memory: 44.6 MB (76.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
-        int windowStart = 0, minLength = Integer.MAX_VALUE, matched = 0, start = 0;
-        Map<Character, Integer> tMap = new HashMap<>();
-        //pehle put all the character to map
-        for(char ch : t.toCharArray()) {
-            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
-        }
-        //now start the sliding
-        for(int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
-            //agar pehle se hai map me to freq ghatao
-            char currChar = s.charAt(windowEnd);
-            if(tMap.containsKey(currChar)) {
-                tMap.put(currChar, tMap.get(currChar) - 1);
-                //ghatane ka baad check if its freq is > 0
-                //if yes ive found a ,matching character
-                if(tMap.get(currChar) >= 0) {
-                    matched += 1;
-                }
-            }
-            //agar all characters are matched
-            while(matched == t.length()) {
-                //and cuurennt length is smaller than previous 
-                if(minLength > (windowEnd - windowStart + 1)) {
-                    minLength = windowEnd - windowStart + 1;
-                    start = windowStart;
-                }
-                char leftChar = s.charAt(windowStart);
-                windowStart += 1;
+         int n = s.length();
 
-                if(tMap.containsKey(leftChar)) {
-                    if(tMap.get(leftChar) == 0) {
-                        matched -= 1;
-                    }
-                    tMap.put(leftChar, tMap.get(leftChar) + 1);
-                }
-            }
-        }
-
-        if(minLength > s.length()) {
+        if (t.length() > n)
             return "";
+
+        Map<Character, Integer> mp = new HashMap<>();
+
+        // store karliya
+        for (char ch : t.toCharArray())
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+
+        int requiredCount = t.length();
+        int i = 0, j = 0;
+
+        int minWindowSize = Integer.MAX_VALUE;
+        int start_i = 0;
+
+        // story starts
+        while (j < n) {
+            char ch = s.charAt(j);
+
+            if (mp.containsKey(ch) && mp.get(ch) > 0)
+                requiredCount--;
+
+            mp.put(ch, mp.getOrDefault(ch, 0) - 1);
+
+            while (requiredCount == 0) {
+                // start shrinking the window
+
+                int currWindowSize = j - i + 1;
+
+                if (minWindowSize > currWindowSize) {
+                    minWindowSize = currWindowSize;
+                    start_i = i;
+                }
+
+                char startChar = s.charAt(i);
+                mp.put(startChar, mp.getOrDefault(startChar, 0) + 1);
+
+                if (mp.containsKey(startChar) && mp.get(startChar) > 0) {
+                    requiredCount++;
+                }
+
+                i++;
+            }
+
+            j++;
         }
-        return s.substring(start, start + minLength);
+
+        return minWindowSize == Integer.MAX_VALUE ? "" : s.substring(start_i, start_i + minWindowSize);
     }
 }
