4bcbdfa59727ab3a4373eeb8b999fa805f9f5138
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-08 16:19:48 +0530

    Time: 17 ms (56.96%) | Memory: 44.4 MB (90.22%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,26 +3,29 @@ class Solution {
         
         int windowStart = 0, minLength = Integer.MAX_VALUE, matched = 0, start = 0;
         Map<Character, Integer> tMap = new HashMap<>();
-
+        //pehle put all the character to map
         for(char ch : t.toCharArray()) {
             tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
         }
-
+        //now start the sliding
         for(int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            //agar pehle se hai map me to freq ghatao
             char currChar = s.charAt(windowEnd);
             if(tMap.containsKey(currChar)) {
                 tMap.put(currChar, tMap.get(currChar) - 1);
+                //ghatane ka baad check if its freq is > 0
+                //if yes ive found a ,matching character
                 if(tMap.get(currChar) >= 0) {
                     matched += 1;
                 }
             }
-
+            //agar all characters are matched
             while(matched == t.length()) {
+                //and cuurennt length is smaller than previous 
                 if(minLength > (windowEnd - windowStart + 1)) {
                     minLength = windowEnd - windowStart + 1;
                     start = windowStart;
                 }
-
                 char leftChar = s.charAt(windowStart);
                 windowStart += 1;
 
