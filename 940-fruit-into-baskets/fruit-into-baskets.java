be23748e71c89fef34453509092d1cd5fa3f575e
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-12 01:36:43 +0530

    Time: 50 ms (57.64%) | Memory: 54.4 MB (95.18%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -34,7 +34,8 @@ class Solution {
 
     while (r < n) {
         hm.put(fruits[r], hm.getOrDefault(fruits[r], 0) + 1);
-        if (hm.size() > 2) {
+        
+            //can use while here but it will take more time
             if (hm.size() > 2) {
                 hm.put(fruits[l], hm.get(fruits[l]) - 1);
                 if (hm.get(fruits[l]) == 0) {
@@ -42,7 +43,7 @@ class Solution {
                 }
                 l++;
             }
-        }
+        
         
         max = Math.max(max, r - l + 1);
         r++;
