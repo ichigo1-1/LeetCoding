427ea5c4f4f0a6f66653bbed1f357d2cfc81585b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-13 14:25:39 +0530

    Time: 3 ms (89.58%) | Memory: 60.9 MB (30.01%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,15 +1,42 @@
 class Solution {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int n = arr.length;
-    int count = 0;
-    for (int i = 0; i <= n - k; i++) {
-        int sum = 0;
-        for (int j = i; j < i + k; j++) {   
-            sum += arr[j];
+    public int numOfSubarrays(int[] nums, int k, int threshold) {
+
+        //BRUTE FORCE
+    // int n = arr.length;
+    // int count = 0;
+    // for (int i = 0; i <= n - k; i++) {
+    //     int sum = 0;
+    //     for (int j = i; j < i + k; j++) {   
+    //         sum += arr[j];
+    //     }
+    //     if (sum / k >= threshold) {
+    //         count++;
+    //     }
+    // }
+    // return count;
+
+
+    //SLDIING WUNDIW
+    int i=0;
+    int j=0;
+    int sum=0;
+    int count=0;
+    int n=nums.length;
+    while(j<n)
+    {
+        sum+=nums[j];
+        if(j-i+1==k)
+        {
+            int len=j-i+1;
+            if(sum/len>=threshold)
+            {
+                count++;
+            }
+            //shrink
+            sum-=nums[i];
+            i++;
         }
-        if (sum / k >= threshold) {
-            count++;
-        }
+        j++;
     }
     return count;
     }
