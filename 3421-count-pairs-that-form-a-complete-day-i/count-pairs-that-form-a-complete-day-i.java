f4388bc761cde8c63884f709b849dafcf63820b2
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-16 15:16:16 +0530

    Time: 2 ms (100.00%) | Memory: 42.9 MB (50.00%) - LeetSync

--- 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.java
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.java
@@ -1,16 +1,31 @@
 class Solution {
     public int countCompleteDayPairs(int[] hours) {
-        int sum=0;
-        for(int i=0;i<hours.length-1;i++)
-        {
-            for(int j=i+1;j<hours.length;j++)
-            {
-                if((hours[i]+hours[j])%24==0)
-                {
-                    sum++;
-                }
+        // int sum=0;
+        // for(int i=0;i<hours.length-1;i++)
+        // {
+        //     for(int j=i+1;j<hours.length;j++)
+        //     {
+        //         if((hours[i]+hours[j])%24==0)
+        //         {
+        //             sum++;
+        //         }
+        //     }
+        // }
+        // return sum;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < hours.length; i++) {
+            int remainder = hours[i] % 24;
+            // Calculate the complement remainder that when added to the current remainder makes a multiple of 24
+            int complement = (24 - remainder) % 24;
+            // Increment count by the number of elements that have the complement remainder
+            if (hm.containsKey(complement)) {
+                count += hm.get(complement);
             }
+            // Update the hashmap with the current remainder
+            hm.put(remainder, hm.getOrDefault(remainder, 0) + 1);
         }
-        return sum;
+        return count;
     }
 }
