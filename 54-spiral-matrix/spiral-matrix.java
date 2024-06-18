300d491db979936b63ea41451b86d5c229b1ab09
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-06-19 01:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (91.27%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -4,41 +4,86 @@ import java.util.List;
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
 
-        List<Integer> ans = new ArrayList<>();
+        // List<Integer> ans = new ArrayList<>();
 
-        int minrow = 0;
-        int mincol = 0;
-        int maxrow = matrix.length;
-        int maxcol = matrix[0].length;
+        // int minrow = 0;
+        // int mincol = 0;
+        // int maxrow = matrix.length;
+        // int maxcol = matrix[0].length;
 
-        //to check all elements 
-        int tne = maxrow * maxcol;
-        int count = 0;
+        // //to check all elements 
+        // int tne = maxrow * maxcol;
+        // int count = 0;
 
-        while (tne > count) {
+        // while (tne > count) {
+        //     //top
+        //     for (int i = mincol; i < maxcol && count < tne; i++) {
+        //         ans.add(matrix[minrow][i]);
+        //         count++;
+        //     }
+        //     minrow++;
+
+        //     //right
+        //     for (int i = minrow; i < maxrow && count < tne; i++) {
+        //         ans.add(matrix[i][maxcol - 1]);
+        //         count++;
+        //     }
+        //     maxcol--;
+
+        //     //bottom
+        //     for (int i = maxcol - 1; i >= mincol && count < tne; i--) {
+        //         ans.add(matrix[maxrow - 1][i]);
+        //         count++;
+        //     }
+        //     maxrow--;
+
+        //     //left
+        //     for (int i = maxrow - 1; i >= minrow && count < tne; i--) {
+        //         ans.add(matrix[i][mincol]);
+        //         count++;
+        //     }
+        //     mincol++;
+        // }
+        // return ans;
+
+        List<Integer>ans=new ArrayList<>();
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int tne=n*m;
+        int minrow=0;
+        int mincol=0;
+        int maxrow=matrix.length-1;
+        int maxcol=matrix[0].length-1;
+        int count=0;
+        while(count<n*m)
+        {
             //top
-            for (int i = mincol; i < maxcol && count < tne; i++) {
+            for(int i=mincol;i<=maxcol&&count<tne;i++)
+            {
                 ans.add(matrix[minrow][i]);
                 count++;
             }
             minrow++;
 
             //right
-            for (int i = minrow; i < maxrow && count < tne; i++) {
-                ans.add(matrix[i][maxcol - 1]);
+            for(int i=minrow;i<=maxrow&&count<tne;i++)
+            {
+                ans.add(matrix[i][maxcol]);
                 count++;
             }
             maxcol--;
 
             //bottom
-            for (int i = maxcol - 1; i >= mincol && count < tne; i--) {
-                ans.add(matrix[maxrow - 1][i]);
+            for(int i=maxcol;i>=mincol&&count<tne;i--)
+            {
+                ans.add(matrix[maxrow][i]);
                 count++;
             }
             maxrow--;
 
             //left
-            for (int i = maxrow - 1; i >= minrow && count < tne; i--) {
+            for(int i=maxrow;i>=minrow&&count<tne;i--)
+            {
                 ans.add(matrix[i][mincol]);
                 count++;
             }
