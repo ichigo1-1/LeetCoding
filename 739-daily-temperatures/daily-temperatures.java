6929aaf22a067c22f1252298d58a04e93a363057
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-07-19 10:57:17 +0530

    Time: 82 ms (16.64%) | Memory: 59.9 MB (72.80%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,16 +1,28 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> s=new Stack<>();
-        int n=temperatures.length;
-        int res[]=new int[n];
-        for(int i=0;i<n;i++){
-            int currTemp=temperatures[i];
-            while(!s.isEmpty() && temperatures[s.peek()]<currTemp){
-                int k=s.pop();
-                res[k]=i-k;
+         Stack<Integer> st = new Stack<>();
+        int n = temperatures.length;
+        int[] res = new int[n];
+        
+        // Iterate from the last day to the first day
+        for (int i = n - 1; i >= 0; i--) {
+            // Pop elements from the stack while the current temperature is greater or equal
+            // to the temperature at the index stored at the top of the stack
+            while (!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
+                st.pop();
             }
-            s.push(i);
+            
+            // If the stack is not empty, the next warmer day is at the index on the top of the stack
+            if (!st.isEmpty()) {
+                res[i] = st.peek() - i;
+            } else {
+                res[i] = 0; // No warmer day in the future
+            }
+            
+            // Push the current day index onto the stack
+            st.push(i);
         }
+        
         return res;
     }
 }
