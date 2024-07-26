59ce148f07150652d17276e9717b0b11936a0358
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-07-26 22:14:36 +0530

    Time: 2 ms (98.83%) | Memory: 57.5 MB (17.46%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -3,26 +3,23 @@ class Solution {
         int n = gas.length;
         int start = 0;
         int gasCount = 0;
+
         int totalGas = 0;
         int totalCost = 0;
         for (int i = 0; i < n; i++) {
             totalGas += gas[i];
             totalCost += cost[i];
         }
+
         if (totalGas < totalCost) {
             return -1;
         }
-        int ans=start;
         for (int i = 0; i < n; i++) {
             gasCount += gas[i] - cost[i];
             if (gasCount < 0) {
                 start = i + 1;
                 gasCount = 0;
             }
-            else{
-                ans=i;
-            }
-            //we are not breaking here because if 1 ans is found no other will be satisfying the cond and at last will return true
         }
         return start;
     }
