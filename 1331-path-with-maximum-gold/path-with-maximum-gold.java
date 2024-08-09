43505d44176b02d980093f96969af5a073bbbcfc
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-08-09 21:32:23 +0530

    Time: 87 ms (34.29%) | Memory: 41.1 MB (59.47%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -0,0 +1,44 @@
+class Solution {
+    int n,m;
+    int[]di={-1,0,1,0};
+    int[]dj={0,1,0,-1};
+    public int solve(int i,int j,int[][]grid)
+    {
+        //base case 
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0)
+        {
+            return 0;
+        }
+        //get the current gold value
+        int ogvalue=grid[i][j];
+        grid[i][j]=0;//to mark it AS TAKEN
+        int maxgold=0;
+        //traverse
+        for(int x=0;x<4;x++)
+        {
+            int nrow=i+di[x];
+            int ncol=j+dj[x];
+            maxgold=Math.max(maxgold,solve(nrow,ncol,grid));
+        }
+        //bring back the old valuel
+        grid[i][j]=ogvalue;
+        return ogvalue+maxgold;
+
+    }
+    public int getMaximumGold(int[][] grid) {
+        n=grid.length;
+        m=grid[0].length;
+        int result=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]!=0)
+                {
+                    result=Math.max(result,solve(i,j,grid));
+                }
+            }
+        }
+        return result;
+    }
+}
