cb98e9dbda1f741c7b3b46e6788a45dfb8174549
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-08-14 02:58:15 +0530

    Time: 43 ms (15.14%) | Memory: 44.1 MB (15.43%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,19 +1,38 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        ArrayList<Integer>as=new ArrayList<>();
-        //add all the friends
+
+        // ArrayList<Integer>as=new ArrayList<>();
+        // //add all the friends
+        // for(int i=1;i<=n;i++)
+        // {
+        //     as.add(i);
+        // }
+        // //Game starts from 1st player which is sitting at index 0 in arr
+        // int i=0;
+        // while(as.size()>1)
+        // {
+        //     int idx=(i+k-1)%as.size();
+        //     as.remove(idx);
+        //     i=idx;
+        // }
+        // return as.get(0);
+
+                        //USING QUEUE
+        Queue<Integer>q=new LinkedList<>();
         for(int i=1;i<=n;i++)
         {
-            as.add(i);
-        }
-        //Game starts from 1st player which is sitting at index 0 in arr
-        int i=0;
-        while(as.size()>1)
+            q.add(i);
+
+        }   
+        while(q.size()>1)
         {
-            int idx=(i+k-1)%as.size();
-            as.remove(idx);
-            i=idx;
-        }
-        return as.get(0);
+            for(int it=1;it<k;it++)
+            {
+                q.add(q.poll());
+            }
+            //now delete the topmost
+            q.poll();
+        }            
+        return q.peek(); 
     }
 }
