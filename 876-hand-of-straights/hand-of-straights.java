0026fcca5349279b69224cf293e3d8cdc75a4237
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-08-16 14:20:18 +0530

    Time: 38 ms (50.35%) | Memory: 46.2 MB (5.30%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,25 +1,25 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize != 0) {
+        if(hand.length%groupSize!=0)
+        {
             return false;
         }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int val : hand) {
-            pq.add(val);
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int it:hand)
+        {
+            pq.add(it);
         }
-
-        while (!pq.isEmpty()) {
-            int current = pq.poll();
-            for (int i = 1; i < groupSize; i++) {
-                if (!pq.remove(current + i)) {
+        while(!pq.isEmpty())
+        {
+            int cur=pq.poll();
+            for(int i=1;i<groupSize;i++)
+            {
+                if(!pq.remove(cur+i))
+                {
                     return false;
                 }
             }
         }
-
         return true;
     }
 }
