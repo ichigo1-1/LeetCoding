237398cb5494563c6a6da223aa8849bf999fd919
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-02 00:54:35 +0530

    Time: 1 ms (48.40%) | Memory: 40.7 MB (57.76%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int len(int n) {
-        String s = String.valueOf(n);
+    public int len(int n)
+    {
+        String s=String.valueOf(n);
         return s.length();
     }
-
     public int addDigits(int num) {
-        int sum = num;
-        while (len(sum) > 1) {  // Check if the number has more than one digit
-            sum = 0;
-            while (num > 0) {   // Calculate the sum of digits of num
-                int rem = num % 10;
-                sum += rem;
-                num /= 10;
+        int sum=num;
+        while(len(sum)>1)
+        {
+            sum=0;
+            while(num>0)
+            {
+                int rem=num%10;
+                sum+=rem;
+                num/=10;
             }
-            num = sum;          // Update num to the new sum to continue the process
+            num=sum;
         }
         return sum;
     }
