81e3a80e3e52efa2f94bc1f32183c8428ad67991
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-01 21:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (27.26%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        //simulation problem 
+        //give it a dry run taking n=20
+        //youll see all the perfect squares are only on
+        return (int)Math.sqrt(n);
+    }
+}
