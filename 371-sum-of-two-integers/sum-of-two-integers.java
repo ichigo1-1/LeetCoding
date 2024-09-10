619fe572707280919b4f051ec3826544726a4b21
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-11 02:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (11.01%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -2,15 +2,16 @@ class Solution {
     public int getSum(int a, int b) {
         //remember the sum TT SOLVED ON NEETCODE
         //NOTE IF THERES A CARRY WE SHIFT CARRY TO THE LEFT BIT
-         int sum_without_carry=a^b;
+        int sum=a^b;
         int carry=a&b;
-        int actual_carry=carry<<1;//just do by taking 1 and 5 as examples 1+1=0 and carry=1 transfers to left bit
+        int actual_carry=carry<<1;
         while(carry!=0)
         {
-            carry=sum_without_carry & actual_carry;
-            sum_without_carry=sum_without_carry^actual_carry;
+            carry=sum&actual_carry;
+            sum=sum^actual_carry;
+            
             actual_carry=carry<<1;
-        }        
-        return sum_without_carry;
         }
+        return sum;
     }
+}
