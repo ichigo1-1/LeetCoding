a0b73857aec3c74d7620b147b739d664eed1a3b9
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-12 12:45:31 +0530

    Time: 2 ms (87.42%) | Memory: 55.9 MB (33.73%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+
+        //          APPROACH 1(TLE)
         // int[]res=new int[nums.length];
         // int ind=0;
         
@@ -17,45 +19,69 @@ class Solution {
         // }
         // return res;
 
-        //           APPROACH 2
+        //           APPROACH 2(USING THE /)
+        // int[]res=new int[nums.length];
+        // int ind=0;
+        // int zeroes=0;
+        // int product=1;
+        // int prod_without_zeroes=1;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]==0)
+        //     {
+        //         zeroes++;
+        //     }
+        //     if(nums[i]!=0)
+        //     {
+        //         prod_without_zeroes*=nums[i];
+        //     }
+        //     product*=nums[i];
+        // }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         if(zeroes>0)
+        //         {
+        //             res[ind++]=0;
+        //         }
+        //         else{
+        //             res[ind++]=product/nums[i];
+        //         }
+        //     }
+        //     else{
+        //         if(zeroes>1)
+        //         {
+        //             res[ind++]=0;
+        //         }
+        //         else{
+        //             res[ind++]=prod_without_zeroes;
+        //         }
+        //     }
+        // }
+        // return res;
+
+        //                  APPROACH 3:- USING PREFIX AND SUFFIX
+        int[]prefix=new int[nums.length];
+        int[]suffix=new int[nums.length];
+        prefix[0]=1;
+        suffix[suffix.length-1]=1;
+        for(int i=1;i<prefix.length;i++)
+        {
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+        for(int i=suffix.length-2;i>=0;i--)
+        {
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+
+        //now fill the resultant array
         int[]res=new int[nums.length];
         int ind=0;
-        int zeroes=0;
-        int product=1;
-        int prod_without_zeroes=1;
+
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==0)
-            {
-                zeroes++;
-            }
-            if(nums[i]!=0)
-            {
-                prod_without_zeroes*=nums[i];
-            }
-            product*=nums[i];
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=0)
-            {
-                if(zeroes>0)
-                {
-                    res[ind++]=0;
-                }
-                else{
-                    res[ind++]=product/nums[i];
-                }
-            }
-            else{
-                if(zeroes>1)
-                {
-                    res[ind++]=0;
-                }
-                else{
-                    res[ind++]=prod_without_zeroes;
-                }
-            }
+            res[ind++]=prefix[i]*suffix[i];
         }
         return res;
     }
