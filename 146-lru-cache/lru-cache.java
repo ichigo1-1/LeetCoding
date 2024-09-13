d1bead468fe8155be6d504264594b0e29389df46
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-09-13 13:29:52 +0530

    Time: 780 ms (6.43%) | Memory: 108.7 MB (98.80%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -9,8 +9,8 @@ class Pair {
 }
 
 class LRUCache {
-    private final ArrayList<Pair> cache = new ArrayList<>();
-    private final int capacity;
+    public  ArrayList<Pair> cache = new ArrayList<>();
+    public  int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
@@ -27,15 +27,16 @@ class LRUCache {
                 return value;
             }
         }
-        return -1; // Key not found
+        return -1; // Key not found so return -1;
     }
 
     public void put(int key, int value) {
         for (int i = 0; i < cache.size(); i++) {
             if (cache.get(i).key == key) {
-                // Remove the old pair
+                // Remove the old pair as we have to update it with newer valaue 
+                //also put at last because it is recently used
                 cache.remove(i);
-                // Add the updated pair to the end
+                // Add the updated pair to the end to mark as recently used.
                 cache.add(new Pair(key, value));
                 return;
             }
@@ -44,8 +45,11 @@ class LRUCache {
         // If the cache is full, remove the least recently used (first) entry
         if (cache.size() == capacity) {
             cache.remove(0);
+            cache.add(new Pair(key, value));
         }
-        // Add the new key-value pair
-        cache.add(new Pair(key, value));
+        else{
+            //as capacity is already there insert directly
+            cache.add(new Pair(key, value));
+        }
     }
 }
