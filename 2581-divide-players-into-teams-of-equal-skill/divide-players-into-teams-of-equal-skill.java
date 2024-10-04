4c9324ff165c21069e02826b583bd69453dc4d62
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-04 11:48:24 +0530

    Time: 18 ms (24.18%) | Memory: 57.8 MB (39.29%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,39 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
-    
-    // Helper function to check if we can form valid pairs with equal sums
-    public boolean isPairwiseEqualSum(int[] arr, int targetSum) {
-        int i = 0;
-        int j = arr.length - 1;
-        
-        while (i < j) {
-            int curSum = arr[i] + arr[j];
-            if (curSum != targetSum) {
-                return false;
+    public boolean check(int[]arr)
+    {
+        int i=1;
+        int j=arr.length-2;
+        int prev=arr[0]+arr[arr.length-1];
+        while(i<=j)
+        {
+            int cur=arr[i]+arr[j];
+            if(cur!=prev)
+            {
+                return false; 
             }
             i++;
             j--;
         }
         return true;
     }
-
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);  // Sort the array
-        
-        // Calculate the target sum for pairs
-        int targetSum = skill[0] + skill[skill.length - 1];
-        long sum = 0;
-        int n = skill.length;
-        
-        // Check if the pairs are valid
-        if (isPairwiseEqualSum(skill, targetSum)) {
-            for (int i = 0; i < n / 2; i++) {
-                sum += skill[i] * skill[n - i - 1];  // Calculate the sum of products of pairs
+        Arrays.sort(skill);
+        long sum=0;
+        int n=skill.length;
+        if(check(skill)==true)
+        {
+            for(int i=0;i<skill.length/2;i++)
+            {
+                sum+=(long)skill[i]*skill[n-i-1];
             }
             return sum;
-        } else {
-            return -1;  // Return -1 if pairing is not possible
+        }
+        else{
+            return -1;
         }
     }
 }
