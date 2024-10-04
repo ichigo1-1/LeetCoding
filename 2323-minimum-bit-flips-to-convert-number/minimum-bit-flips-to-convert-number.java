0191465992ef2c80c77574700ce87cd36f80e796
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-05 00:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (6.82%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,15 +1,27 @@
 class Solution {
+   public int check(int n) {
+    int c = 0;
+    while (n > 0) {
+        // If the rightmost bit is 1, increment the count
+        if ((n & 1) == 1) {
+            c++;
+        }
+        // Shift n to the right by 1 bit (equivalent to dividing by 2)
+        n = n >> 1;
+    }
+    return c;
+}
     public int minBitFlips(int start, int goal) {
         int xored=start^goal;
         //count ones
         int count=0;
-        for(int i=0;i<32;i++)
-        {
-            if(((xored&(1<<i))!=0))
-            {
-                count++;
-            }
-        }
-        return count;
+        // for(int i=0;i<32;i++)
+        // {
+        //     if(((xored&(1<<i))!=0))
+        //     {
+        //         count++;
+        //     }
+        // }
+        return check(xored);
     }
 }
