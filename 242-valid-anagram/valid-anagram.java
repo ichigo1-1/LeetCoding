deefe3efed4e604845e5c6b01fd467cc67bc9c35
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-04 12:59:14 +0530

    Time: 12 ms (33.41%) | Memory: 44.4 MB (47.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        String a = sortString(s);
-        String b=sortString(t);
-        return a.equals(b);
-
-    }
-    public static String sortString(String str) {
-        // Convert the string to a character array
-        char[] charArray = str.toCharArray();
+        // String a = sortString(s);
+        // String b=sortString(t);
+        // return a.equals(b);
         
-        // Sort the character array
-        Arrays.sort(charArray);
-        
-        // Convert the sorted character array back to a string
-        return new String(charArray);
+        Map<Character,Integer>hm=new HashMap<>();
+        //put all element count
+        for(char ch:s.toCharArray())
+        {
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+        }
+        //decrease count
+        for(char ch:t.toCharArray())
+        {
+            hm.put(ch,hm.getOrDefault(ch,0)-1);
+        }
+        //last check to check wheter sb 0 hua ki nahi
+        for(char ch:hm.keySet())
+        {
+            if(hm.get(ch)!=0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
