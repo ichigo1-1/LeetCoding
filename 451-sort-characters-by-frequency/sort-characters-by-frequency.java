a183ddbecf0264ed6b2361b333e5720565ffac1b
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-04 13:12:11 +0530

    Time: 19 ms (25.57%) | Memory: 45.9 MB (16.72%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,32 +1,20 @@
 class Solution {
     public String frequencySort(String s) {
- 
-        HashMap<Character,Integer> hm = new HashMap<>();
-        int len=s.length();
-        for(int i=0;i<len;i++)
+        TreeMap<Character,Integer>hm=new TreeMap<>();
+        for(char ch:s.toCharArray())
         {
-            char ch=s.charAt(i);
             hm.put(ch,hm.getOrDefault(ch,0)+1);
         }
-        //step2. create a list and sort it
-        List<Character>list=new ArrayList<>(hm.keySet());
-        //own compartor to sort in dec order
-        list.sort((a,b)->hm.get(b)-hm.get(a));
-        //now run a loop till char length and append in answer;
-        StringBuilder sb = new StringBuilder();
-        for(char ch:list)
+        List<Character>ls=new ArrayList<>(hm.keySet());
+        ls.sort((a,b)->hm.get(b)-hm.get(a));
+        StringBuilder sb=new StringBuilder();
+        for(char ch:ls)
         {
-            //ch ka length tak chalo by doing hm.get(ch) as hm me count hai uska
             for(int i=0;i<hm.get(ch);i++)
             {
                 sb.append(ch);
             }
         }
         return sb.toString();
-
-
-        
-
-        
     }
 }
