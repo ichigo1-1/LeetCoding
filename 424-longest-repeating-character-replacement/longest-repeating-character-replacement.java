e2b77c4d2aaf4f159df898d7456fba1bc702482a
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-04 21:22:44 +0530

    Time: 27 ms (25.94%) | Memory: 44.2 MB (28.85%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -7,28 +7,18 @@ class Solution {
         HashMap<Character,Integer>hm=new HashMap<>();
         while(j<n)
         {
-             char cur = s.charAt(j);
-            
-            // Update frequency of the current character
-            hm.put(cur, hm.getOrDefault(cur, 0) + 1);
-            
-            // Update maxfreq (the most frequent character in the current window)
-            maxfreq = Math.max(maxfreq, hm.get(cur));
-            
-            // Calculate the number of changes needed to make the current window all the same character
-            int len = j - i + 1;
-            int changes = len - maxfreq;
-            
-            // If the number of changes exceeds k, shrink the window
-            if (changes > k) {
-                char start = s.charAt(i);
-                hm.put(start, hm.get(start) - 1);
+            char current=s.charAt(j);
+            hm.put(current,hm.getOrDefault(current,0)+1);
+            int len=j-i+1;
+            maxfreq=Math.max(maxfreq,hm.get(current));
+            int changes=len-maxfreq;
+            if(changes>k)
+            {
+                char start=s.charAt(i);
+                hm.put(start,hm.get(start)-1);
                 i++;
             }
-            
-            // Update the result with the maximum valid window size
-            ans = Math.max(ans, j - i + 1);
-            
+            ans=Math.max(ans,j-i+1);
             j++;
         }
         return ans;
