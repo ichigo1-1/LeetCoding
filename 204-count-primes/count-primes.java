71bd2c8f3d22215e7c372e32bdcede2fcabb5055
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-05 15:30:52 +0530

    Time: 99 ms (62.80%) | Memory: 64.2 MB (11.14%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -2,10 +2,10 @@ class Solution {
     public int countPrimes(int n) {
         int[]arr=new int[n];
         //calculate
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(arr[i]==0)
             {
-                for(int j=2*i;j<n;j+=i)
+                for(int j=i*i;j<n;j+=i)
                 {
                     arr[j]=1;//they can never be prime
                 }
