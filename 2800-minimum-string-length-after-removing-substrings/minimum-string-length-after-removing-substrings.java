8bcc3db023d20d4e90f388852c54f34c27f13458
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-07 22:54:13 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (50.13%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,17 +1,42 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character>st=new Stack<>();
-        st.push(s.charAt(0));
-        for(int i=1;i<s.length();i++)
+        // Stack<Character>st=new Stack<>();
+        // st.push(s.charAt(0));
+        // for(int i=1;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if (!st.isEmpty() && ((st.peek() == 'A' && ch == 'B') || (st.peek() == 'C' && ch == 'D'))) {
+        //         st.pop();
+        //     }
+        //     else{
+        //         st.push(ch);
+        //     }
+        // }
+        // return st.size();
+
+
+        //2 pointer technique
+        int i=0;//write;
+        int j=1;//read;
+        char[]ch=s.toCharArray();
+        int n=s.length();
+        while(j<n)
         {
-            char ch=s.charAt(i);
-            if (!st.isEmpty() && ((st.peek() == 'A' && ch == 'B') || (st.peek() == 'C' && ch == 'D'))) {
-                st.pop();
+            if(i<0)
+            {
+                i++;
+                ch[i]=ch[j];
+            }
+            else if((ch[j]=='B' && ch[i]=='A')||(ch[j]=='D' && ch[i]=='C'))
+            {
+                i--;
             }
             else{
-                st.push(ch);
+                i++;
+                ch[i]=ch[j];
             }
+            j++;
         }
-        return st.size();
+        return i+1;
     }
 }
