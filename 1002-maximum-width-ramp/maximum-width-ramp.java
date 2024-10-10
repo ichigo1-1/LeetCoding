cc1182aadbfd235ef07672e9f96929cc13bf1135
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-11 00:43:25 +0530

    Time: 5 ms (82.84%) | Memory: 54.7 MB (40.55%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -30,7 +30,31 @@ class Solution {
         //     st.push(i);
         // }
         // return ans;
-        int n = nums.length;
+        // int n = nums.length;
+
+        // // Create an array to store the maximum values from the right
+        // int[] maxRight = new int[n];
+        // maxRight[n - 1] = nums[n - 1];
+        
+        // // Fill the maxRight array
+        // for (int i = n - 2; i >= 0; i--) {
+        //     maxRight[i] = Math.max(maxRight[i + 1], nums[i]);
+        // }
+        // int ramp = 0;
+        // int i = 0;
+        // int j = 0;
+
+        // // Find the maximum width ramp
+        // while (j < n) {
+        //     while (i < j && nums[i] > maxRight[j]) {
+        //         i++;
+        //     }
+        //     ramp = Math.max(ramp, j - i);
+        //     j++;
+        // }
+
+        // return ramp;
+       int n = nums.length;
 
         // Create an array to store the maximum values from the right
         int[] maxRight = new int[n];
