32913ef529fca72d7ff4bb2d6af6f17d721c22db
Ashutosh Anand <94797565+ichigo1-1@users.noreply.github.com>
2024-10-15 14:17:46 +0530

    Time: 2 ms (99.86%) | Memory: 45.3 MB (81.33%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,68 +1,37 @@
 class Solution {
     public int candy(int[] ratings) {
-        // int[]left=new int[ratings.length];
-        // // int[]right=new int[ratings.length];
+        int n=ratings.length;
 
-        // //step 1:- fill the left
-        // left[0]=1;
-        // for(int i=1;i<ratings.length;i++)
-        // {
-        //     if(ratings[i]>ratings[i-1])
-        //     {
-        //         left[i]=left[i-1]+1;
-        //     }
-        //     else{
-        //         left[i]=1;
-        //     }
-        // }
-
-        // // //step 2:- fill the right;
-        // right[ratings.length-1]=1;
-        // for(int i=ratings.length-2;i>=0;i--)
-        // {
-        //     if(ratings[i]>ratings[i+1])
-        //     {
-        //         right[i]=right[i+1]+1;
-        //     }
-        //     else{
-        //         right[i]=1;
-        //     }
-        // }
-        // //step 3:- Get the maximium of left and right for thaat index
-        // int res=0;
-        // for(int i=0;i<ratings.length;i++)
-        // {
-        //     res+=Math.max(left[i],right[i]);
-        // }
-        // return res; 
-        
-
-
-        //BETERR SOLUTION**********************
-        int n = ratings.length;
-        int[] left = new int[n];
-
-        //step 1:- fill the left
-        left[0] = 1;
-        for(int i = 1; i < n; i++) {
-            if(ratings[i] > ratings[i-1]) {
-                left[i] = left[i-1] + 1;
-            } else {
-                left[i] = 1;
+        int[]left = new int[n];
+        int[]right = new int[n];
+        left[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]=left[i-1]+1;
+            }
+            else{
+                left[i]=1;
             }
         }
-
-        //step 2 and 3
-        int res = Math.max(1,left[n-1]);
-        int right = 1;
-        for(int i = n - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i+1]) {
-                right = right + 1;
-            } else {
-                right = 1;
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                right[i]=right[i+1]+1;
             }
-            res += Math.max(left[i],right);
+            else{
+                right[i]=1;
+            }
         }
-        return res; // add the candy for the first child
+        //now sum
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=Math.max(left[i],right[i]);
+        }
+        return sum;
     }
 }
